8435ebbfaef4cb05c97d3797d4c4f19a8fa2fd79
JiaHao <we5759456@gmail.com>
2024-06-23 20:29:22 +0800

    綠界0623

--- server/controllers/loginControllers.js
+++ server/controllers/loginControllers.js
@@ -2,16 +2,17 @@ const { raw } = require('mysql');
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('zeabur', 'root', 'q7sHPXWh6ln8YB2rfVIJa0e159t3pcZ4', {
     dialect: 'mysql',
-    host: 'hkg1.clusters.zeabur.com',
-    port: 30395,
+    host: 'mysql.zeabur.internal',
+    port: 3306,
     dialectOptions: {
         connectTimeout: 60000 // 以毫秒为单位增加连接超时时间
     },
     pool: {
-        max: 10, // 最大連接數
-        min: 0, // 最小連接數
-        acquire: 30000, // 獲取連接的超時時間（毫秒）
-        idle: 10000 // 連接閒置時間（毫秒）超過這個時間連接將被釋放
+        max: 15,
+        min: 5,
+        idle: 20000,
+        evict: 15000,
+        acquire: 30000
     }
 });
 const UserModle = require('../../models/user');
