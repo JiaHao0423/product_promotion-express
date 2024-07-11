8c94fa24c649f67286cf37abb2e58f8dfd5f0f55
JiaHao <we5759456@gmail.com>
2024-07-11 16:15:38 +0800

    綠界0711

--- server/routes/pay.js
+++ server/routes/pay.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const cartController = require('../controllers/payControllers');
+const payController = require('../controllers/payControllers');
 
 
-router.get('/checkout',cartController.checkout)
-router.post('/checkout-data',cartController.data)
-router.get('/checkout-confirmation',cartController.confirm)
-router.post('/create-order',cartController.create)
-// router.get('/payment',cartController.pay)
-router.post('/payment-ECPay',cartController.processPayment)
-router.post('/order-completion',cartController.completion)
-router.post('/pay-return',cartController.return)
+router.get('/checkout',payController.checkout)
+router.post('/checkout-data',payController.data)
+router.get('/checkout-confirmation',payController.confirm)
+router.post('/create-order',payController.create)
+// router.get('/payment',payController.pay)
+router.post('/payment-ECPay',payController.processPayment)
+router.get('/order-completion',payController.completion)
+router.post('/pay-return',payController.return)
 
 module.exports = router;
